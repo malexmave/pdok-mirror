098bc1d1c3cc0810d177ca80494aaa735220c437
Max <max@velcommuta.de>
2017-01-15 16:41:54 +0100

    Add AGPL header to all files

--- controller/scraper.py
+++ controller/scraper.py
@@ -1,5 +1,21 @@
 # -*- encoding: utf-8 -*-
-"""Scraper for the pdoc file structures."""
+"""Scraper for the pdoc file structures.
+
+Copyright (C) 2017  Max Maass
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 from util.util import _download_tuple, get_html, pdf_to_text
 from models.database import Wahlperiode, Plenarprotokoll, Drucksache
